795f8e5f01b9c5329d60ee3b3c2b0726bbb938d9
Katynicole89 <Katynicole89@hotmail.com>
2024-02-04 15:11:12 -0600

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -22,14 +22,15 @@ let fight = function(enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0,playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack letiable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack)
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -48,7 +49,8 @@ let fight = function(enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack letiable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -82,7 +84,7 @@ let startGame = function() {
       let pickedEnemyName = enemyNames[i];
 
       // reset enemyHealth before starting new fight
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       // pass the pickedEnemyName letiable's value into the fight function, where it will assume the value of the enemyName parameter
       fight(pickedEnemyName);
@@ -180,5 +182,10 @@ let shop = function() {
   }
 };
 
+let randomNumber = function(min, max){
+    let value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
+};
 // start first game when page loads
 startGame();
